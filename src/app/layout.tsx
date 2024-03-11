fd8e6bb86e6722c4ee03fa1d763a0b29dba22ffa
박서영 <seokachuu@gmail.com>
2024-03-11 21:11:12 +0900

    Feat: 보일러플레이트 세팅

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'My TodoList',
+  description: '투두리스트, my todo list',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}

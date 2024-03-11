3f3e1dea3ebc8faaf4fb4bbe351d15e8ed83c711
박서영 <seokachuu@gmail.com>
2024-03-11 22:12:18 +0900

    Feat: form 영역 완성

--- src/components/todos/TodoForm.tsx
+++ src/components/todos/TodoForm.tsx
@@ -1,11 +1,74 @@
-import React from 'react'
+'use client';
+import InputForm from '@/hooks/inputForm';
+import { randomCardColor } from '@/util/color';
+import React, { FormEvent } from 'react';
 
 const TodoForm = () => {
-  return (
-    <div>
-      폼영역
-    </div>
-  )
-}
+  const { formState, onChangeHandler, resetForm } = InputForm({
+    id: '',
+    title: '',
+    content: '',
+    createAt: new Date(),
+    isdone: false,
+    color: '',
+  });
 
-export default TodoForm
+  const onSubmitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const date = new Date();
+    if (validation()) {
+      const newTodos = {
+        id: crypto.randomUUID(),
+        title,
+        content,
+        createAt: date,
+        isdone: false,
+        color: randomCardColor(),
+      };
+      alert('입력되었습니다');
+    }
+  };
+
+  //validation
+  const validation = () => {
+    if (!title.trim()) {
+      alert('제목을 입력해 주세요');
+      return;
+    }
+    if (!content.trim()) {
+      alert('내용을 입력해 주세요');
+      return;
+    }
+    return true;
+  };
+
+  const { title, content } = formState;
+  return (
+    <form onSubmit={onSubmitHandler}>
+      <p>
+        <label htmlFor="title">제목</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={title}
+          onChange={onChangeHandler}
+          autoFocus
+        />
+      </p>
+      <p>
+        <label htmlFor="content">내용</label>
+        <input
+          type="text"
+          id="content"
+          name="content"
+          value={content}
+          onChange={onChangeHandler}
+        />
+      </p>
+      <button>등록하기</button>
+    </form>
+  );
+};
+
+export default TodoForm;
---
+++ src/hooks/interface.ts
@@ -0,0 +1,8 @@
+export interface IInputForm {
+  id: string;
+  title: string;
+  content: string;
+  createAt: Date;
+  isdone: boolean;
+  color: string;
+}

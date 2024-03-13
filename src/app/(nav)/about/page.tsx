f787b701e0b489d8f327148ec7016bbe9e4bfc07
박서영 <seokachuu@gmail.com>
2024-03-13 16:53:25 +0900

    Feat: repoat 구현 완료

--- src/app/(nav)/about/page.tsx
+++ src/app/(nav)/about/page.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 const about = async () => {
   const response = await fetch(`http://localhost:4000/companyInfo`);
   const data = await response.json();
-  console.log(data);
 
   return (
     <div>
       <h1>CompanyInfo</h1>
-      <div>{data.name}</div>
-      <div>{data.desctiption}</div>
+      <div>
+        <p>{data.name}</p>
+        <p>{data.desctiption}</p>
+      </div>
     </div>
   );
 };
--- src/app/(nav)/report/page.tsx
+++ src/app/(nav)/report/page.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const report = () => {
-  return <div>report페이지입니다.</div>;
+const report = async () => {
+  const response = await fetch(`http://localhost:4000/todos`, {
+    next: {
+      revalidate: 10,
+    },
+  });
+
+  const data = await response.json();
+
+  return (
+    <div>
+      <p>
+        현재까지 <span>{data.length}</span>개의 투두리스트가 등록되었습니다.
+      </p>
+      <p>현재까지 4개의 완료 리스트, 6개의 할일 리스트가 존재합니다.</p>
+    </div>
+  );
 };
 
 export default report;
